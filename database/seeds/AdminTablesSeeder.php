d3c49a314cb7ef247260ff161463845624480a13
luo <luo@luos-Mac-mini.local>
2019-12-26 12:05:49 +0800

    init

---
+++ database/seeds/AdminTablesSeeder.php
@@ -0,0 +1,244 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // base tables
+        Encore\Admin\Auth\Database\Menu::truncate();
+        Encore\Admin\Auth\Database\Menu::insert(
+            [
+                [
+                    "parent_id" => 0,
+                    "order" => 1,
+                    "title" => "Dashboard",
+                    "icon" => "fa-bar-chart",
+                    "uri" => "/",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 2,
+                    "title" => "Admin",
+                    "icon" => "fa-tasks",
+                    "uri" => "",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 3,
+                    "title" => "Users",
+                    "icon" => "fa-users",
+                    "uri" => "auth/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 4,
+                    "title" => "Roles",
+                    "icon" => "fa-user",
+                    "uri" => "auth/roles",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 5,
+                    "title" => "Permission",
+                    "icon" => "fa-ban",
+                    "uri" => "auth/permissions",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 6,
+                    "title" => "Menu",
+                    "icon" => "fa-bars",
+                    "uri" => "auth/menu",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 7,
+                    "title" => "Operation log",
+                    "icon" => "fa-history",
+                    "uri" => "auth/logs",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 7,
+                    "title" => "Helpers",
+                    "icon" => "fa-gears",
+                    "uri" => "",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 8,
+                    "order" => 8,
+                    "title" => "Scaffold",
+                    "icon" => "fa-keyboard-o",
+                    "uri" => "helpers/scaffold",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 8,
+                    "order" => 9,
+                    "title" => "Database terminal",
+                    "icon" => "fa-database",
+                    "uri" => "helpers/terminal/database",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 8,
+                    "order" => 10,
+                    "title" => "Laravel artisan",
+                    "icon" => "fa-terminal",
+                    "uri" => "helpers/terminal/artisan",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 8,
+                    "order" => 11,
+                    "title" => "Routes",
+                    "icon" => "fa-list-alt",
+                    "uri" => "helpers/routes",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "项目",
+                    "icon" => "fa-bars",
+                    "uri" => "projects",
+                    "permission" => "*"
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "任务",
+                    "icon" => "fa-bars",
+                    "uri" => "tasks",
+                    "permission" => "*"
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 12,
+                    "title" => "Config",
+                    "icon" => "fa-toggle-on",
+                    "uri" => "config",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 13,
+                    "title" => "Log viewer",
+                    "icon" => "fa-database",
+                    "uri" => "logs",
+                    "permission" => NULL
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Permission::truncate();
+        Encore\Admin\Auth\Database\Permission::insert(
+            [
+                [
+                    "name" => "All permission",
+                    "slug" => "*",
+                    "http_method" => "",
+                    "http_path" => "*"
+                ],
+                [
+                    "name" => "Dashboard",
+                    "slug" => "dashboard",
+                    "http_method" => "GET",
+                    "http_path" => "/"
+                ],
+                [
+                    "name" => "Login",
+                    "slug" => "auth.login",
+                    "http_method" => "",
+                    "http_path" => "/auth/login\r\n/auth/logout"
+                ],
+                [
+                    "name" => "User setting",
+                    "slug" => "auth.setting",
+                    "http_method" => "GET,PUT",
+                    "http_path" => "/auth/setting"
+                ],
+                [
+                    "name" => "Auth management",
+                    "slug" => "auth.management",
+                    "http_method" => "",
+                    "http_path" => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs"
+                ],
+                [
+                    "name" => "Admin helpers",
+                    "slug" => "ext.helpers",
+                    "http_method" => "",
+                    "http_path" => "/helpers/*"
+                ],
+                [
+                    "name" => "Admin Config",
+                    "slug" => "ext.config",
+                    "http_method" => "",
+                    "http_path" => "/config*"
+                ],
+                [
+                    "name" => "Logs",
+                    "slug" => "ext.log-viewer",
+                    "http_method" => "",
+                    "http_path" => "/logs*"
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Role::truncate();
+        Encore\Admin\Auth\Database\Role::insert(
+            [
+                [
+                    "name" => "Administrator",
+                    "slug" => "administrator"
+                ]
+            ]
+        );
+
+        // pivot tables
+        DB::table('admin_role_menu')->truncate();
+        DB::table('admin_role_menu')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "menu_id" => 2
+                ],
+                [
+                    "role_id" => 1,
+                    "menu_id" => 13
+                ],
+                [
+                    "role_id" => 1,
+                    "menu_id" => 14
+                ]
+            ]
+        );
+
+        DB::table('admin_role_permissions')->truncate();
+        DB::table('admin_role_permissions')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "permission_id" => 1
+                ]
+            ]
+        );
+
+        // finish
+    }
+}
