aae9978964c465dff7e4b97fd768acc76887ed6d
luochenjie <547605677@qq.com>
2019-12-27 11:20:31 +0800

    修改首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+    <link rel="shortcut icon" href="{{asset('icons/site.png')}}" type="image/x-icon" />
+
     <!-- Styles -->
     <style>
         html, body {
